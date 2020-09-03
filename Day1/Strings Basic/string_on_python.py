88bc847224cf36d06c9bc1f108120557eb956e18
unknown <nawarajb5official@gmail.com>
2020-09-03 10:24:20 +0545

    Basic string operation in python

---
+++ Day1/Strings Basic/string_on_python.py
@@ -0,0 +1,5 @@
+my_name = "Nawaraj"
+family_name = "Basnet"
+country = "Nepal"
+my_experience = "I am enjoying 30 days of python"
+print('My name is '+my_name+' '+family_name+' and I live in '+country+' '+my_experience)
