82dcea684d90b5c97f762e4600929b5395842ff5
unknown <nawarajb5official@gmail.com>
2020-09-03 10:54:36 +0545

    basic string concept in python

--- Day1/Strings Basic/string_on_python.py
+++ Day1/Strings Basic/string_on_python.py
@@ -2,5 +2,5 @@
 my_name = "Nawaraj"
 family_name = "Basnet"
 country = "Nepal"
-my_experience = "I am enjoying 30 days of python"
+my_experience = "I am enjoying 30 days of python and this is my first day"
 print('My name is '+my_name+' '+family_name+' and I live in '+country+' '+my_experience)
