52ca2013f6280329e9faddc5e9036b0a72bf552f
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 23:54:06 +0545

    Comparison of length of jargon and python

---
+++ Day 3/comparison_jargon_python.py
@@ -0,0 +1,6 @@
+#Find the length of 'python' and 'jargon' and make a falsy comparison statement.
+a = 'python'
+b = 'jargon'
+print('The length of python is {} and length of jargon is {}'.format(len(a),len(b) ))
+false_comparison =len(a)!=len(b)
+print(false_comparison)
