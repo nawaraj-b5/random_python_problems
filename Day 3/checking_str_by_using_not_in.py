d48f4bc8ee48a0bc0cf812a2d0ba6536a46e92a5
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 23:57:34 +0545

    checking the string by using  not in for comparison

---
+++ Day 3/checking_str_by_using_not_in.py
@@ -0,0 +1,8 @@
+#There is no 'on' in both dragon and python
+dragon = 'dragon'
+python ='python'
+on_not_in_dragon = 'on' not in dragon
+on_not_in_python = 'on' not in python
+print("The statement that 'on' is not found in dragon is  :",on_not_in_dragon  )
+print("The statement that 'on' is not found in python is  :",on_not_in_python  )
+print("The statement that 'on' is not found in both dragon and python is  :",on_not_in_python and on_not_in_dragon  )
