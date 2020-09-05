f21011059228c71b7f23bdc14297744c19b2017e
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 17:00:48 +0545

    Updated to make code look simpler

--- Day 2/5. Comparison of names using len()/name_comparison.py
+++ Day 2/5. Comparison of names using len()/name_comparison.py
@@ -6,8 +6,8 @@ length_of_last_name = len(last_name_input)
 first_name_has_more_length = length_of_first_name > length_of_last_name
 last_name_has_more_length = length_of_last_name >length_of_first_name
 both_have_equal_length =  length_of_first_name > length_of_last_name
-print ("The length of first name is : {}".format( length_of_first_name) )
-print("The length of last name is : {}".format(length_of_last_name) )
-print("First name has more length : {}".format(first_name_has_more_length) )
-print("Last name has more length : {}".format(last_name_has_more_length) )
-print("First name and Last name have equal length :{}".format(both_have_equal_length) )
+print ("The length of first name is : ", length_of_first_name )
+print("The length of last name is : ",length_of_last_name )
+print("First name has more length : ",first_name_has_more_length )
+print("Last name has more length : ",last_name_has_more_length )
+print("First name and Last name have equal length : ",both_have_equal_length )
