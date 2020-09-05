195f73adeaec572c49c230da8465378ad3679e9f
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 23:56:15 +0545

    checking the string by using in for comparison

---
+++ Day 3/checking_by_using_in.py
@@ -0,0 +1,8 @@
+#Use and operator to check if 'on' is found in both 'python' and 'jargon'
+a = 'python'
+b = 'jargon'
+on_in_python = 'on'in a
+on_in_jargon = 'on'in b
+print("The statement that 'on' is found in python is  :",on_in_python  )
+print("The statement that 'on' is found in jargon is  :",on_in_jargon )
+print("The statement that 'on' is found in both jargon and python  is  :",on_in_jargon and on_in_python  )
