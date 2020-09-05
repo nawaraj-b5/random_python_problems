66b29e8d405e3e4ad3d801c6fbae04431d1a81a8
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-06 00:01:09 +0545

    Updated code

--- Day 3/checking_by_using_in.py
+++ Day 3/checking_by_using_in.py
@@ -6,3 +6,8 @@ on_in_jargon = 'on'in b
 print("The statement that 'on' is found in python is  :",on_in_python  )
 print("The statement that 'on' is found in jargon is  :",on_in_jargon )
 print("The statement that 'on' is found in both jargon and python  is  :",on_in_jargon and on_in_python  )
+
+#I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+sentence = 'I hope this course is not full of jargon'
+jargon_in_sentence = 'jargon' in sentence
+print("The statement that 'jargon' is found in 'I hope this course is not full of jargon' is  :",on_in_python  )
