0e8d31bcc8e3097f62e7cb20f7ea6a2db01b9abe
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 22:08:19 +0545

    Declaration of different variables with different data type

---
+++ Day 3/various_variable_declaration.py
@@ -0,0 +1,4 @@
+# Declare your age as integer , height as float variable and complex_nummber as complex number variable
+age = 24                #Declare your age as integer variable
+height = 5.5            #Declare your height as a float variable
+complex_number = 1+2j   #Declare a complex number variable
