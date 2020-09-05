9dae0b7b41cf58586246d5383c21f7abd48e9bd0
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 23:20:07 +0545

    Calculation of the output from given mathematical formula

---
+++ Day 3/calculation_from_formula_provided.py
@@ -0,0 +1,4 @@
+#Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+x = int( input( 'Enter the value of x : ' ) )
+y = (x ** 2)+( 6 * x )+9
+print('The value of y is ',y)
