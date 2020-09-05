997ab6c1039cfe71c8af6c89524f17407a09740c
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 16:58:42 +0545

    Updated to make code look simpler

--- Day 2/3. variables_with_arithmatic_operation/operation_using_variables.py
+++ Day 2/3. variables_with_arithmatic_operation/operation_using_variables.py
@@ -8,10 +8,10 @@ variable_division = num_two / num_one        #Divide num_one by num_two and assi
 variable_reminder = num_two % num_one        #Use modulus division to find num_two divided by num_one and assign the value to a variable _remainder
 variable_exp = num_one ** num_two           #Calculate num_one to the power of num_two and assign the value to a variable _exp
 variable_floor_division = num_one // num_two   #Find floor division of num_one by num_two and assign the value to a variable _floor_division
-print( "Total : {}".format(variable_total) )
-print( "Difference : {}".format(variable_diff) )
-print( "Product : {}".format(variable_product) )
-print( "Division : {}".format(variable_division) )
-print( "Reminder : {}".format(variable_reminder) )
-print( "Exponent : {}".format(variable_exp) )
-print( "Floor division : {}".format(variable_floor_division) )
+print( "Total : ",variable_total )
+print( "Difference : ",variable_diff )
+print( "Product : ",variable_product )
+print( "Division : ",variable_division )
+print( "Reminder : ",variable_reminder )
+print( "Exponent : ",variable_exp )
+print( "Floor division : ",variable_floor_division )
