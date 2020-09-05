2dde13aa9ccc162c47036387c2c8ada3751dfe36
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-05 23:59:20 +0545

    conversion of string length from int to float and string

---
+++ Day 3/datatype_conversion.py
@@ -0,0 +1,5 @@
+#Find the length of the text python and convert the value to float and convert it to string
+length_of_python = ( len('python') )
+length_of_python_in_float = float(length_of_python)
+length_of_python_in_string = str( length_of_python )
+print ('Length of the python in integer is {}, float is {}, and string is {} '.format(length_of_python,length_of_python_in_float,length_of_python_in_string))
