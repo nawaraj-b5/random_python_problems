32420dd3f0ca4beb7a8d2d8e291d87394d74af9b
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-06 20:56:56 +0545

    calculation of pay of the person on the basis of rate and hours

---
+++ Day 3/pay_of_the_person.py
@@ -0,0 +1,5 @@
+#Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+hours = input('Enter hours : ')
+rpr = input('Enter rate per hour')
+weekly_earning = int(hours) * int( rpr )
+print('The weekly earning is :',weekly_earning)
