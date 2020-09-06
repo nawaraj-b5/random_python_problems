eda0fffdbfafd1583bdac43a870efecc51c711e1
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-06 21:00:56 +0545

    checking even number by help of boolean

---
+++ Day 3/basic_even_checker_boolean.py
@@ -0,0 +1,3 @@
+#Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+number = int( input("Enter the number : ") )
+print ('The number you entered is even : ',number%2 == 0 )
