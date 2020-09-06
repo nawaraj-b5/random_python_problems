d22c88ac53e11e4fb5e6ccb193f7109fddfb5631
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-06 20:59:20 +0545

    comparision with boolean

---
+++ Day 3/comparision_with_boolean_output.py
@@ -0,0 +1,10 @@
+#The floor division of 7 by 3 is equal to the int converted value of 2.7.
+floor_div = 7//3
+floor_div_int =float(floor_div)
+print ('The floor division of 7 by 3 is equal to the int converted value of 2.7 : ',7//3 ==2.7)
+
+#Check if type of '10' is equal to 10
+print( 'The type of 10 and 10.0 is : ',type(10) == type(10.0) )
+
+#Check if int('9.8') is equal to 10
+print( 'int(9.8) and 10 are equal : ',int(9.8) == 10 )
