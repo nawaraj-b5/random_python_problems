22db0b1cacea9af0634774e683aecc8435b3d8c9
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-06 21:01:48 +0545

    calculating slope from given data

---
+++ Day 3/finding_slope.py
@@ -0,0 +1,9 @@
+#Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point (6,10)
+x1 = 2
+y1 = 2
+x2 = 6
+y2 = 10
+distance_between_x = x2-x1
+distance_between_y = y2-y1
+m =  distance_between_y / distance_between_x
+print('The slope of the given point ({}, {}) and ({}, {}) is {}'.format(x1,y1,x2,y2,m))
