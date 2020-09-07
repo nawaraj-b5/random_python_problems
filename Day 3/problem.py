5c0623d67abdf1de8303b3f3ac625f8c87e388f6
Nawaraj Basnet <nawarajb5official@gmail.com>
2020-09-07 23:57:03 +0545

    Calculate the sum of the number from the list using fuction

---
+++ Day 3/problem.py
@@ -0,0 +1,14 @@
+#Write a function that takes a list of integers as a parameter and returns the sum of the list.
+
+def item_list(list_of_integer):
+    result = 0
+    i= 0
+    length =len(list_of_integer)
+    while i < length:
+        result+=list_of_integer[i]
+        i+=1
+    print('The sum of the {} number in the list is {}'.format(length,result))
+    
+item_list([2,3,5,7,8,4,8,5])
+item_list([2,3,0,7,8,4,5])
+
